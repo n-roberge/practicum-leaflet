d5c651b19934175ca86933488a637437924d0129
n-roberge <58243482+n-roberge@users.noreply.github.com>
2022-04-30 18:55:13 -0500

    final version update

--- map/js/main.js
+++ map/js/main.js
@@ -1,4 +1,5 @@
-var map = L.map('map').setView([5, 20.15], 3.25);
+var map;
+map = L.map('map').setView([5, 20.15], 3.4);
 
 //Basemap
 var basemap = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
@@ -8,28 +9,25 @@ var basemap = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{
     zoomOffset: -1,
     accessToken: 'pk.eyJ1IjoiZG9yYWt1NDE3IiwiYSI6ImNrNTR3Y2czbDBucmszanI1bnI2dWRvcjkifQ.sJB2t5gxDp0I5dJBNkJ5DA'
 });
-
-
 basemap.addTo(map);
 
 //ACLED data
-$.getJSON("data/acled.geojson", function(data) {
-    var clusters = L.markerClusterGroup();
-    var acledData = L.geoJson(data, {
-      pointToLayer: function(feature, latlng) {
-        var marker = L.marker(latlng);
-        marker.bindPopup('Event type: ' + feature.properties.event_type + ': ' + feature.properties.sub_event_type + '<br/>' + 'Date: ' + feature.properties.event_date + '<br/>' + 'Country: ' + feature.properties.country + '<br/>' + 'Location: ' + feature.properties.admin1 + ' , ' + feature.properties.admin2 + '<br/>' + 'Actor 1: ' + feature.properties.actor1 + '<br/>' + 'Actor 2: ' + feature.properties.actor2 + '<br/>' + 'Notes: ' + feature.properties.notes);
-        return marker;
-      },
-      onEachFeature: function (feature, layer) {
-        layer.addTo(clusters);
-      }
-    });
-
-  map.addLayer(clusters);
-});
-
-  var controlLayers = L.control.layers().addTo(map);
+function createAcledOverlay(data, layerName){
+  var clusters = L.markerClusterGroup();
+  var acledOverlay = L.geoJson(data, {
+    pointToLayer: function(feature, latlng) {
+      var marker = L.marker(latlng);
+      marker.bindPopup('Event type: ' + feature.properties.event_type + ': ' + feature.properties.sub_event_type + '<br/>' + 'Date: ' + feature.properties.event_date + '<br/>' + 'Country: ' + feature.properties.country + '<br/>' + 'Location: ' + feature.properties.admin1 + ' , ' + feature.properties.admin2 + '<br/>' + 'Actor 1: ' + feature.properties.actor1 + '<br/>' + 'Actor 2: ' + feature.properties.actor2 + '<br/>' + 'Notes: ' + feature.properties.notes);
+      return marker;
+    },
+    onEachFeature: function (feature, layer) {
+      layer.addTo(clusters);
+    }
+  });
+  // map.addLayer(clusters);
+  layerControl.addOverlay(clusters, layerName)
+}
+$.getJSON("data/ACLED_2.geojson", function(data) { createAcledOverlay(data, "Armed Conflict & Location Data")});
 
 //IMR data
 function getColor(d) {
@@ -41,19 +39,40 @@ function getColor(d) {
                    '#DADAEB' ;
 };
 
-//TODO get popup to work)
-let data = $.getJSON("data/Africa_IMR_FeaturesToJSON.geojson");
-L.geoJSON(data, {
-  style: function (feature) {
-    return{
-      fillColor: getColor(feature.properties.IMR),
-      weight: 1,
-      opacity: 1,
-      color: 'white',
-      dashArray: '2',
-      fillOpacity: 0.7
-    };
-  }
-}).bindPopup(function (layer) {
-    return "IMR per 100k: " + layer.feature.properties.IMR
-}).addTo(map)
+function addIMRdata (imrData, layerName){
+  var imrOverlay =L.geoJson(imrData, {
+    style: function (feature) {
+      return {
+        fillColor: getColor(feature.properties.IMR),
+        weight: 1,
+        opacity: 1,
+        color: 'white',
+        dashArray: '2',
+        fillOpacity: 0.6
+      };
+    }
+    }).bindPopup(function (layer) {
+        return "IMR per 1000: " + layer.feature.properties.IMR
+  })
+  layerControl.addOverlay(imrOverlay, layerName)
+}
+
+$.getJSON("data/africa_imr.json", function(imrData) { addIMRdata(imrData, "Infant Mortality Rate")});
+
+//Drought Raster
+var drought = L.imageOverlay("data/drought_africa_1.png", [[75.5,-180.5],[-65.5,180.5]])
+
+//Potential Areas of Concern Raster
+var potentialAreas = L.imageOverlay("data/potentialAreas.png", [[75.5,-180],[-65.5,180]]).addTo(map);
+
+//Rainfed Crops Raster
+var rainCrops = L.imageOverlay("data/cropped_areas_2.png", [[75.5,-180],[-65.5,180]])
+
+//layer control
+var overlays = {
+  "Susceptible Areas": potentialAreas,
+  "Drought Prediction": drought,
+  "Rainfed Crops": rainCrops
+}
+
+var layerControl = L.control.layers(null, overlays).addTo(map);
